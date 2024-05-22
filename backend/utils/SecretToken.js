143029553115c36ef774290f8c649487e8555329
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-05-22 16:28:58 +0100

    Update createSecretToken function in SecretToken.js to include username parameter

--- backend/utils/SecretToken.js
+++ backend/utils/SecretToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
-export const createSecretToken = (id) => {
-  return jwt.sign({id}, process.env.TOKEN_KEY, {expiresIn: 3 * 24 * 60 * 60});
+export const createSecretToken = (id, username) => {
+  return jwt.sign({id, username}, process.env.TOKEN_KEY, {expiresIn: 3 * 24 * 60 * 60});
 };
 
 
