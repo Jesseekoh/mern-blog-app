ba5144af6c220246110dcd4ac3bb07baea51b4d0
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-05-22 16:45:16 +0100

    Add logout functionality and route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  redirect,
+} from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import MainLayout from './layout/MainLayout';
 import Blogs from './pages/Blogs';
 import Login from './pages/Login';
+import Logout from './pages/Logout';
 import Signup from './pages/Signup';
+
 import 'react-toastify/dist/ReactToastify.css';
+import CreatePost from './pages/CreatePost';
 const routes = createBrowserRouter([
   {
     path: '/login',
@@ -16,6 +23,25 @@ const routes = createBrowserRouter([
     element: <Signup />,
   },
   {
+    path: '/logout',
+    element: <Logout />,
+    loader: async () => {
+      try {
+        const response = await fetch('http://localhost:8000/logout', {
+          credentials: 'include',
+        });
+
+        const data = await response.json();
+        if (data) {
+          return redirect('/');
+        }
+        return null;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+  {
     path: '/',
     element: <MainLayout />,
     children: [
@@ -23,6 +49,10 @@ const routes = createBrowserRouter([
         // path: '/',
         index: true,
         element: <Blogs />,
+      },
+      {
+        path: '/create-post',
+        element: <CreatePost />,
       },
     ],
   },
