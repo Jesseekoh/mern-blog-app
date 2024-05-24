5db42b39fcb8bd43fbf51076798f859c9cf13dc5
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-05-24 17:48:35 +0100

    Add routes for logging out, getting all posts and for getting a single post

--- backend/routes/Route.js
+++ backend/routes/Route.js
@@ -1,17 +1,34 @@
-import {Login, SignUp} from '../controllers/AuthController.js'
+import {login, signup, logout, verifyUserWithCookie} from '../controllers/AuthController.js'
 import express from 'express'
 import {getMe} from '../controllers/UserController.js'
 import { verifyToken } from '../middlewares/AuthMiddleware.js'
+import { createBlogPost, getAllBlogs, getBlog } from '../controllers/BlogController.js'
 const router = express.Router()
 
 // Auth routes
-router.post("/signup", SignUp)
-router.post('/login', Login)
+router.post("/signup", signup)
+router.post('/login', login)
+router.get('/logout', logout)
+// verifies user with cookie without credentials
+router.get('/verify-user-with-cookie', verifyUserWithCookie);
 
 
 
 // get blogs
 // get user profile
-// 
 router.get('/profile/:userId', verifyToken, getMe)
+
+
+// TODO: create route for editing blog post
+
+
+
+// Blog routes
+// create post
+router.post('/blog/create', verifyToken, createBlogPost)
+// get all blog posts
+router.get('/blogs/all', getAllBlogs)
+// fetch post
+router.get('/blogs/post/:blogId', getBlog)
+
 export default router
