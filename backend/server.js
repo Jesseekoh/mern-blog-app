5ce6c75c0913ddd6d61ca9421f71f806aee87156
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-05-26 20:10:33 +0100

    Update server.js to include CORS configuration and body-parser middleware

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,40 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import router from './routes/Route.js'
-import mongoose from 'mongoose'
-import { verifyToken } from './middlewares/AuthMiddleware.js'
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import router from './routes/Route.js';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
-const PORT = process.env.PORT || 8000
-const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/mern_blog'
+const PORT = process.env.PORT || 8000;
+const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/mern_blog';
 // const {PORT, DB_URI} = process.env
-const app = express()
+const app = express();
 
 mongoose
   .connect(DB_URI, {})
   .then(() => console.log('MongoDB is connected successfully'))
-  .catch((err) => console.log(err))
+  .catch((err) => console.log(err));
 
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:8000',
+      'http://localhost:5173',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use(cookieParser());
 
-app.use(cors({
-  credentials: true,
-  origin: ['http://localhost:8000', 'http://localhost:5173', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-}))
-
-app.use(cookieParser())
-app.use(express.json())
-
-app.use('/', router)
+app.use('/', router);
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
-})
+  console.log(`Server is listening on port ${PORT}`);
+});
